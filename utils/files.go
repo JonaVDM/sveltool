4444c8d236cac44bc3f3a553807528270af4045c
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-03-19 20:53:17 +0100

    Updated createFolder to create a lot more

--- utils/files.go
+++ utils/files.go
@@ -8,7 +8,7 @@ import (
 func CreateFolder(folder string) error {
 	_, err := os.Stat(folder)
 	if errors.Is(err, os.ErrNotExist) {
-		err := os.Mkdir(folder, os.ModePerm)
+		err := os.MkdirAll(folder, os.ModePerm)
 		if err != nil {
 			return err
 		}
